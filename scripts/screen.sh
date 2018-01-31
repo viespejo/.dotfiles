2f00873a6e6d3091d1a3d2bb6cd6a270d2331602
Vicente Espejo <viespejo@gmail.com>
2018-01-31 10:09:09 +0100

    Screen layouts

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -2,25 +2,30 @@
 
 #Feed this script either:
 #	"l" for laptop screen only,
-#	"v" for vga screen only,
-#	or "d" for dual vga/laptop.
+#	"w" for work screen layout,
+#	or "h" for house screen layout.
 
-d() { if [[ $(xrandr -q | grep VGA1\ con) ]]
-	then param $1
-	else echo "No VGA input detected."
-	fi ;}
-dual() { xrandr --output EDP1 --auto --output VGA1 --auto --right-of EDP1 ;}
-laptop() { xrandr --output EDP1 --auto --output VGA1 --off ;}
-hdmi() { xrandr --output HDMI1 --auto --output VGA1 --off ;}
-vga() { xrandr --output VGA1 --auto --output EDP1 --off ;}
+d() {
+    # if [[ $(xrandr -q | grep VGA1\ con) ]]
+	# then param $1
+	# else echo "No VGA input detected."
+	# fi
+    param $1
+}
+home() { xrandr --output EDP1 --auto --output VGA1 --auto --right-of EDP1 ;}
+laptop() {
+    [ -f ~/.screenlayout/laptop.sh ] && source ~/.screenlayout/laptop.sh
+}
+work() {
+    [ -f ~/.screenlayout/work.sh ] && source ~/.screenlayout/work.sh
+}
 #mirror() { xrandr --addmode VGA1 $lapres && xrandr --output LVDS1 --mode $lapres --output VGA1 --mode $lapres ;}
 
 param() {
 case $1 in
-	d) dual ;;
-	v) vga ;;
+	h) dual ;;
+	w) work ;;
 	l) laptop ;;
-	h) hdmi ;;
 	*) echo -e "Invalid parameter. Add one of the following:\n\"d\" for dualscreen laptop and VGA.\n\"l\" for laptop only\n\"v\" for VGA only.\n\"h\" for HDMI only.";
 esac ;}
 d $1
