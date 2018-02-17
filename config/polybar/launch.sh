cad67dc89b5cac02a5a3740b0cf514f2f1258a1c
Vicente Espejo <viespejo@gmail.com>
2018-02-17 18:56:57 +0100

    New wallpaper

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -11,7 +11,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 #polybar example &
 
 for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top -c ~/.config/polybar/space & MONITOR=$i polybar bottom -c ~/.config/polybar/space & done
-feh --bg-fill ~/.config/wallpapers/space.png
+feh --bg-fill ~/.config/wallpapers/arch_grey.jpg
 . ~/.config/i3/tmpscreen.sh
 
 # for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
